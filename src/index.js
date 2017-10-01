9fcfdaf2dc4b57ba88805d2f06ce573ed2e0c423
Firtina Ozbalikci <firtina14@gmail.com>
2017-10-01 09:43:06 +0100

    Move exports into index

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import * as ReactFiberReconciler from './renderers/shared/fiber/ReactFiberReconciler';
+import * as ReactDOMFrameScheduling from './renderers/shared/ReactDOMFrameScheduling';
+import * as ReactFiberDevToolsHook from './renderers/shared/fiber/ReactFiberDevToolsHook';
+
+export {
+  ReactFiberReconciler,
+  ReactDOMFrameScheduling,
+  ReactFiberDevToolsHook,
+};
